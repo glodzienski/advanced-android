ab08e004e8bc09aa27d324cd567ec1c66c826e4c
glodzienski <crystopher.glodzienski@gmail.com>
2019-03-25 20:12:23 -0300

    Sensores mais uma activity vazia

--- TwentyFive/app/src/main/java/com/example/twentyfive/MainActivity.kt
+++ TwentyFive/app/src/main/java/com/example/twentyfive/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.example.twentyfive
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    // lateinit permite eu declarar a variavel e instanciar ele mais tarde
+    lateinit var texto: TextView
+
+    val sensorManager: SensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        texto.text = event!!.values.zip("XYZ".toList()).fold(""){
+            acc, pair ->
+            "$acc${pair.second}: ${pair.first}\n"
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        texto = txtView
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        sensorManager.unregisterListener(this)
     }
 }
