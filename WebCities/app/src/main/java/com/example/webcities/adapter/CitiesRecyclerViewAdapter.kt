d45e70f2c74c9405c128dfe4653a05ec86a283a2
glodzienski <crystopher.glodzienski@gmail.com>
2019-03-30 16:24:59 -0300

    - Alterados alguns ids padrões criados. - Novo icone. - Separado adapter da class de listagem. (Organização)

---
+++ WebCities/app/src/main/java/com/example/webcities/adapter/CitiesRecyclerViewAdapter.kt
@@ -0,0 +1,73 @@
+package com.example.webcities.adapter
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.webcities.R
+import com.example.webcities.entities.City
+import com.example.webcities.fragment.CityDetailFragment
+import com.example.webcities.ui.CityDetailActivity
+import com.example.webcities.ui.CityListActivity
+import kotlinx.android.synthetic.main.city_list_content.view.*
+
+class CitiesRecyclerViewAdapter (
+    private val parentActivity: CityListActivity,
+    private val values: List<City>,
+    private val twoPane: Boolean
+) : RecyclerView.Adapter<CitiesRecyclerViewAdapter.ViewHolder>() {
+
+    private val onClickListener: View.OnClickListener
+
+    init {
+        onClickListener = View.OnClickListener { v ->
+            val item = v.tag as City
+            if (twoPane) {
+                val fragment = CityDetailFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(CityDetailFragment.ARG_ITEM_ID, item.id)
+                    }
+                }
+                parentActivity.supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.city_detail_container, fragment)
+                    .commit()
+
+                return@OnClickListener
+            }
+
+            val intent = Intent(v.context, CityDetailActivity::class.java).apply {
+                putExtra(CityDetailFragment.ARG_ITEM_ID, item.id)
+            }
+            v.context.startActivity(intent)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.city_list_content, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.name
+
+        with(holder.itemView) {
+            tag = item
+            setOnClickListener(onClickListener)
+        }
+    }
+
+    override fun getItemCount() = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val idView: TextView = view.id_text
+        val contentView: TextView = view.content
+    }
+}
--- WebCities/app/src/main/java/com/example/webcities/ui/CityDetailActivity.kt
+++ WebCities/app/src/main/java/com/example/webcities/ui/CityDetailActivity.kt
@@ -22,7 +22,7 @@ class CityDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_city_detail)
         setSupportActionBar(detail_toolbar)
 
-        fab.setOnClickListener { view ->
+        btnNew.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
