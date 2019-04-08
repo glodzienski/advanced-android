c45f7d95c9a1bd041007c1c11632be614b5ebd8a
glodzienski <crystopher.glodzienski@gmail.com>
2019-04-08 17:42:02 -0300

    Removido classes e xmls de tela não usada mais no projeto.

--- WebCities/app/src/main/java/com/example/webcities/ui/MainActivity.kt
+++ WebCities/app/src/main/java/com/example/webcities/ui/MainActivity.kt
@@ -3,17 +3,11 @@ package com.example.webcities.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.webcities.R
-import com.example.webcities.entities.City
-import com.example.webcities.fragments.CityFragment
 
-class MainActivity : AppCompatActivity(), CityFragment.OnListFragmentInteractionListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onListFragmentInteraction(item: City?) {
-        item.hashCode()
     }
 }
