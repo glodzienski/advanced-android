35801af66d50b083cf934f1a1796a55533d8c91a
glodzienski <crystopher.glodzienski@gmail.com>
2019-05-05 01:37:39 -0300

    Alterado nome da pasta principal do projeto, para um nome mais apropriado com o contexto do mesmo

--- WebMoments/app/src/main/java/com/example/webmoments/util/ImageBuilderUtil.kt
+++ WebMoments/app/src/main/java/com/example/webmoments/util/ImageBuilderUtil.kt
@@ -4,7 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 
 object ImageBuilderUtil {
-    fun prepare(urlpath: String, width: Int = 200, height: Int = 200): Bitmap {
+    fun prepare(urlpath: String, width: Int = 200, height: Int = 200): Bitmap? {
         val bmOptions = BitmapFactory.Options()
         bmOptions.inJustDecodeBounds = true
         BitmapFactory.decodeFile(urlpath, bmOptions)
@@ -15,6 +15,12 @@ object ImageBuilderUtil {
         bmOptions.inJustDecodeBounds = false
         bmOptions.inSampleSize = scaleFactor
 
-        return BitmapFactory.decodeFile(urlpath, bmOptions)
+        val retorno: Bitmap
+        try {
+            retorno = BitmapFactory.decodeFile(urlpath, bmOptions)
+        } catch (e: Exception) {
+            return null
+        }
+        return retorno
     }
 }
